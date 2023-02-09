2b0dfb9bb8869871f2a7d58324eddeffb3203e03
LazuliKao <LazuliKao@outlook.com>
2023-02-09 18:33:49 +0800

    sync

--- Sync.fsx
+++ Sync.fsx
@@ -35,12 +35,24 @@ let download(branch:string)(target:string)=
         //progress.Report(b/c);
         progress.Tick($"{b}/{c} {a}")
     )
+    let callbackt=ProgressHandler(fun str->
+        //System.Console.CursorLeft<-System.Console.BufferWidth-1
+        //for i=0 to System.Console.BufferWidth do printf "\b"
+        //printf "%d / %d\t%s\t%d%%" b c a (b/c)
+        //progress.Report(b/c);
+        progress.Tick(str)
+        true
+    )
+    let callupdatetips=UpdateTipsHandler(fun str _ _->
+        progress.Tick(str)
+        true
+    )
     //let targetGit="https://github.com/LiteLDev/SDK-cpp.git"
     //https://gitclone.com/github.com/LiteLDev/SDK-cpp.git
     //let targetGit="https://gitclone.com/github.com/LiteLDev/SDK-cpp.git"
     let targetGit="https://ghproxy.com/https://github.com/LiteLDev/SDK-cpp.git"
     //let targetGit="https://hub.njuu.cf/LiteLDev/SDK-cpp.git"
-    let res=LibGit2Sharp.Repository.Clone(targetGit,betaPath,CloneOptions(BranchName=branch,OnCheckoutProgress=callback))
+    let res=LibGit2Sharp.Repository.Clone(targetGit,betaPath,CloneOptions(BranchName=branch,OnProgress=callbackt,OnCheckoutProgress=callback,OnUpdateTips=callupdatetips))
     printfn "\n%s" res
     let tp=Path.Combine(currentPath,target)
     printfn "Target : %s" tp
@@ -49,8 +61,9 @@ let download(branch:string)(target:string)=
         if dir.EndsWith(".git")|>not then
             let name=Path.GetFileName(dir)
             let target=Path.Combine(tp,name)
-            if Directory.Exists(target) then Directory.Delete(target,true)
-            Directory.Move(dir,target)
+            if Directory.Exists(target) then Directory.Delete(target,true) 
+            if Path.GetFileName(dir).StartsWith "_git2" |> not then 
+                Directory.Move(dir,target)
 let downloadTask(branch:string)(target:string)=
     async{
         download branch target
@@ -58,7 +71,7 @@ let downloadTask(branch:string)(target:string)=
 [
     downloadTask "develop" "Develop"
     downloadTask "main" "Release"
-    downloadTask "beta" "Beta"
+    //downloadTask "beta" "Beta"
 ]
 |>Async.Parallel
 |>Async.RunSynchronously
